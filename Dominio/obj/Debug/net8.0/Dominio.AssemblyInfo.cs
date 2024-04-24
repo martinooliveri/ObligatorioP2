6ff17727d66e41e0ec46ace3076ad3ec72007582
Martino Oliveri <mo310622@fi365.ort.edu.uy>
2024-04-23 21:56:31 -0300

    z

--- Dominio/obj/Debug/net8.0/Dominio.AssemblyInfo.cs
+++ Dominio/obj/Debug/net8.0/Dominio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dominio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ee281cf90cf91fa724485a69882b42026d14208")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c373e5dbf787b04cf122a63bf46871b35b2cef80")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dominio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dominio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
